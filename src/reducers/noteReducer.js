def01be95f8d202a53fb7dc1ab630e54d8c0f8ba
Andy-git985 <andy.yu617@gmail.com>
2022-11-01 21:08:10 -0400

    uncontrolled forms

--- src/reducers/noteReducer.js
+++ src/reducers/noteReducer.js
@@ -1,7 +1,7 @@
 const noteReducer = (state = [], action) => {
   switch (action.type) {
     case 'NEW_NOTE':
-      return state.concat(action.data);
+      return [...state, action.data];
     case 'TOGGLE_IMPORTANCE': {
       const id = action.data.id;
       const noteToChange = state.find((n) => n.id === id);
@@ -16,4 +16,4 @@ const noteReducer = (state = [], action) => {
   }
 };
 
-module.exports = noteReducer;
+export default noteReducer;
